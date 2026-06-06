af40b238eed968a90e3119d54c60a2186aed04ac
illahristuk <novytskiyillia@gmail.com>
2026-06-06 20:34:30 +0200

    fix

--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     REDIS_URL: str = "redis://localhost:6379/0"
 
     DATABASE_URL: str
+    CICD_DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/ci_cd_database"
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
